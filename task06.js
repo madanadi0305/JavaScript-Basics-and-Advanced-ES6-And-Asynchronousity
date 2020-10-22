116032ed36e6da6d3447d6a28c01ea154e38edf3
madanadi0305 <madanadi0305@gmail.com>
2020-10-22 17:25:18 +0000

    Update task06.js

---
+++ task06.js
@@ -0,0 +1,28 @@
+function filter(array,callback){
+    return callback(array); 
+}
+function filter10(array){
+for(var i=0;i<array.length;i++){
+    if(array[i]===10){
+        array.splice(i,1);
+    }
+}
+return array;
+}
+
+function filter10(array){
+    for(var i=0;i<array.length;i++){
+        if(array[i]===5){
+        array.splice(i,1);
+        }
+    }
+return array;
+}
+
+
+
+module.exports = {
+  ten: filter10,
+  five: filter5,
+  fil: filter
+};
