44bd8b6aa3144f365e373c5eca184adc14bcca48
madanadi0305 <madanadi0305@gmail.com>
2020-10-23 04:08:32 +0000

    Update task06.js

--- task06.js
+++ task06.js
@@ -18,15 +18,8 @@ return array;
 }
 
 function filter(array,callback){
-    var choice='five';
-    if(choice){
-    var new_array=filter5(array);
-    return callback(new_array);
-    }
-    else{
-        var new_array=filter10(array);
-        return callback(new_array);
-    } 
+
+ return callback(array);   
 }
 
 module.exports = {
