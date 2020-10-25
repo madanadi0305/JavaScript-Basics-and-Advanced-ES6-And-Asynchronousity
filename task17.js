ec1a6192a9482882f801c605477a67113120848b
madanadi0305 <madanadi0305@gmail.com>
2020-10-25 12:40:33 +0000

    Update Wiki/35-task17.md, task17.js files

---
+++ task17.js
@@ -0,0 +1,41 @@
+var fs=require('fs');
+
+let readFile=(fname)=>{
+return new Promise((resolve,reject)=>{
+fs.readFile((err,data)=>{
+if(err===false){
+    resolve(data);
+}
+
+else{
+    reject(err);
+}
+
+});
+
+
+
+});
+
+}
+
+let writeFile=(fname,s)=>{
+return new Promise((resolve,reject)=>{
+fs.writeFile(fname,s,(err)=>{
+if(err===false){
+    resolve("Done");
+}
+else{
+    reject("Failed");
+}
+});
+
+});
+
+};
+
+
+module.exports = {
+  readFile: readFile,
+  writeFile: writeFile
+};
