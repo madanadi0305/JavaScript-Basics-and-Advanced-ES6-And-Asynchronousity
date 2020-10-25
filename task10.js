0dbd0c0951e83feedb6b52eb0473fbe36e36013c
madanadi0305 <madanadi0305@gmail.com>
2020-10-25 04:46:20 +0000

    Update Wiki/26-task10.md, task10.js files

---
+++ task10.js
@@ -0,0 +1,25 @@
+let bina=(x)=>{
+return new Promise((resolve,reject)=>{
+var n=parseInt(x);
+if(x>0){
+    resolve("YES");
+}
+else{
+    reject("NO");
+}
+
+});
+
+
+};
+var m=1;
+bina.then(m=>{
+ console.log("YES");   
+})
+
+
+
+
+module.exports = {
+  bina: bina
+};
