c44c85d71fa126a74aa342b929487905182ea39f
madanadi0305 <madanadi0305@gmail.com>
2020-10-25 05:03:55 +0000

    Update task11.js, Wiki/27-task11.md files

---
+++ task11.js
@@ -0,0 +1,23 @@
+let sum=(x,y)=>{
+return new Promise((resolve,reject)=>{
+if(parseInt(x)>0){
+    var sum=x+y;
+    resolve(sum);
+}
+else{
+    reject("x should be greater than 0");
+}
+
+});
+
+};
+var m=1;
+var n=2;
+sum(m,n)
+.then(result=>{console.log(result);})
+.catch(err=>{console.log(err)});
+
+
+
+
+module.exports = { add: add };
