e0956d97777412f5b595ab1bf5c17b6f6619fb31
madanadi0305 <madanadi0305@gmail.com>
2020-10-26 05:20:02 +0000

    Update task17.js

--- task17.js
+++ task17.js
@@ -1,41 +1,44 @@
 var fs=require('fs');
-
 function readFile(fname){
 return new Promise((resolve,reject)=>{
-fs.readFile((err,data)=>{
-if(err===false){
-    resolve(data.toString());
-}
-
-else{
+fs.readFile(fname,(err,data)=>{
+if(err){
     reject("No file found");
+    
+}
+else{
+    resolve(data);
 }
 
 });
 
 
-
 });
 
-};
+
+}
 
 function writeFile(fname,s){
 return new Promise((resolve,reject)=>{
-fs.writeFile(fname,s,(err,data)=>{
-if(err===false){
-    resolve("Done");
-}
-else{
+fs.writeFile(fname,s,(err)=>{
+if(err){
     reject("Failed");
 }
-});
+
+else{
+    resolve("Done!");
+}
 
 });
 
-};
+
+});
 
 
-module.exports = {
-  readFile: readFile,
-  writeFile: writeFile
+}
+
+module.exports={
+readFile:readFile,
+writeFile:writeFile
 };
+
