372d0ec9c277f905b409332c952fd993bdb1b817
madanadi0305 <madanadi0305@gmail.com>
2020-10-26 05:29:52 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -1,30 +1,24 @@
-//var ar1=[1,2,3,4];
-//var ar2=[0];
-let arrpush=(arr1,arr2)=>{
+let arrpush=(ar1,ar2)=>{
 return new Promise((resolve,reject)=>{
 setTimeout(()=>{
-if(arr2.length>1){
-    for(var i=0;i<arr1.length;i++){
-    arr2.push(arr1[i]);
-
-    }
-    
-    resolve(arr2);
+if(ar2.length>1){
+    ar2.push(ar1);
+    resolve(ar2);
 }
 else{
     reject("Error!");
 }
+
 },1000);
+
 
 });
 
+
 };
-arrpush(ar1,ar2).then(result=>{console.log(result);}).catch(result=>{console.log(result);});
 
-
-
-module.exports = {
-  ar1: ar1,
-  ar2: ar2,
-  arrpush: arrpush
+module.exports={
+ar1:ar1,
+ar2:ar2,
+arrpush:arrpush
 };
