7b3c59233a80d5cc4c29f60a776931c2b0cce807
madanadi0305 <madanadi0305@gmail.com>
2020-10-27 11:28:23 +0000

    Update task20.js

--- task20.js
+++ task20.js
@@ -10,14 +10,22 @@ res(str1.toString());
 }
 
 async function func2(str2){
-var str22=await func1(str2);
-return str22.toString();
+//var str22=await func1(str2);
+return str2.toString();
 }
 
 function concatenated(s1,s2){
-return func1(s1)+func2(s2);
+return new Promise((resolve)=>{
+var str_final=func1(s1)+func2(s2);
+resolve(str_final);
+
+});
+
+//return func1(s1)+func2(s2);
 
 }
+
+
 
 module.exports={
     concatenated:concatenated
