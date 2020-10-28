85c9018a7590746881a09159b0618aef99b75d50
madanadi0305 <madanadi0305@gmail.com>
2020-10-28 16:30:38 +0000

    Update Wiki/50-task28.md, task28.js files

---
+++ task28.js
@@ -0,0 +1,34 @@
+async function timeout(x){
+    return new Promise(res=>{
+   setTimeout(function(){
+       res(x.toString()+"ms timer finished");
+   },parseInt(x));
+
+    });
+}
+
+async function resolveAfter2Second(){
+console.log("starting slow promise");
+var result2=await timeout(2000);
+return result2;
+}
+
+async function resolveAfter1Second(){
+console.log("starting fast promise");
+var result1=await timeout(1000);
+return result1;
+}
+
+async function concurrentStart(){
+var promises=[];
+console.log("==CONCURRENT START with await==");
+var resultFast=await resolveAfter1Second();
+var resultSlow=await resolveAfter2Second();
+
+promises.push(resultSlow);
+promises.push(resultFast);
+return Promises.all(promises);
+}
+
+
+module.exports=concurrentStart;
