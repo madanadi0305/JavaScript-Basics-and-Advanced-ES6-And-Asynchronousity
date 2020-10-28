7c21515d959c72800c28e6695d54eee087c183e0
madanadi0305 <madanadi0305@gmail.com>
2020-10-28 16:46:01 +0000

    Update task24.js, Wiki/46-task24.md files

---
+++ task24.js
@@ -0,0 +1,39 @@
+var fs=require('fs');
+function readFile(path){
+return new Promise((resolve,reject)=>{
+fs.readFile(path,'utf-8',(err,data)=>{
+if(err){
+    reject("Error Occured!");
+}
+else{
+    resolve(data);
+}
+});
+
+});
+
+}
+
+async function count(){
+ var counts={};   
+ var dataContents=await readFile(path);
+ var wordsArray=dataContents.split(" ");
+ for(var i=0;i<wordsArray.length;i++){
+  var word=wordsArray[i];
+  if(!counts[word]){
+      counts[word]=1;
+
+  }
+
+  else{
+      counts[word]=counts[word]+1;
+  }
+
+ }
+ return count;   
+} 
+
+
+module.exports = {
+  counter: count
+};
