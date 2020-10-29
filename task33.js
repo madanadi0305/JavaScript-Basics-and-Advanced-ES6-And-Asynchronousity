657f3876c52657597adf8f8a3ed275cf505467fc
madanadi0305 <madanadi0305@gmail.com>
2020-10-29 12:37:03 +0000

    Update task33.js, Wiki/58-task33.md files

---
+++ task33.js
@@ -0,0 +1,23 @@
+function * generateOddSeries(number){
+var n=parseInt(number);
+for(var i=0;i<n;n++){
+    if(i%2!==0){
+        yield i;
+    }
+}
+
+}
+
+for(var odd of generateOddSeries()){
+    console.log(odd);
+}
+
+
+
+
+
+
+module.exports={
+oddNumberFn:generateOddSeries
+
+};
