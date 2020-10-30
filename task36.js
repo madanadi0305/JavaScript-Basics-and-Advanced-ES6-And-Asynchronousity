c4b162289232bbb0f92f23ee9ca1ab725c87d526
madanadi0305 <madanadi0305@gmail.com>
2020-10-30 05:27:44 +0000

    Update Wiki/62-task36.md, task36.js files

---
+++ task36.js
@@ -0,0 +1,25 @@
+function filterFn(array){
+var i=0;
+while(i<array.length){
+    if(array[i]%2===0){
+        array[i]=0;
+        yield array;
+    }
+    
+    i=i+1;
+}
+
+}
+
+for(var filter of filterFn([1,2,3,4,5,6,7,8,9,10])){
+    console.log(filter);
+}
+
+
+
+
+
+module.exports={
+filterFn:filterFn
+
+}
