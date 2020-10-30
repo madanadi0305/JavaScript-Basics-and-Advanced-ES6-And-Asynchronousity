70626e67c08e67ad8aa88bd65fa870bac879196d
madanadi0305 <madanadi0305@gmail.com>
2020-10-30 04:58:33 +0000

    Update task34.js, Wiki/59-task34.md files

---
+++ task34.js
@@ -0,0 +1,30 @@
+function * fibonacciFn(){
+var a=0;
+var b=1; 
+var c;
+var sum=0;
+yield a;
+yield b;
+
+while(true){
+c=a+b;
+sum=c;
+a=b;
+b=c;
+yield sum;
+if(sum>=50 && sum<=60){
+    break;
+}
+}
+
+
+}
+
+
+for(fib of fibonacciFn()){
+    console.log(fib);
+}
+
+module.exports={
+  fibonacciFn:fibonacciFn  
+}
