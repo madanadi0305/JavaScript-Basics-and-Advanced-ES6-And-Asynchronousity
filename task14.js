5102acc8ca2fba7ff3c4c14bd389cecd301c8fbc
madanadi0305 <madanadi0305@gmail.com>
2020-11-01 04:49:42 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -5,7 +5,8 @@ function arrpush(array1,array2){
 return new Promise((res,rej)=>{
 setTimeout(()=>{
 if(array2.length>1){
-    res(array2.push(array1));
+    (array2.push(array1));
+    res(array2);
 }
 else{
     rej("Error!");
@@ -20,7 +21,8 @@ else{
 
 }
 
-arrpush(ar1,ar2).then(res=>{console.log(res);}).catch(err=>{console.log(err.message)});
+arrpush(ar1,ar2).then(res=>{console.log(res);});
+//.catch(err=>{console.log(err.message)});
 /*async function arrpush(ar1,ar2){
 var result=await arrpush(ar1,ar2);
 return result;
