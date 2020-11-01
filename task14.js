33e0def4c4310836f327021772e8655c28a49e98
madanadi0305 <madanadi0305@gmail.com>
2020-11-01 05:12:03 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -21,7 +21,7 @@ else{
 
 }
 
-arrpush(ar1,ar2).then(res=>{console.log(res);}).catch(res=>{console.log(res)});
+//arrpush(ar1,ar2).then(res=>{console.log(res);}).catch(res=>{console.log(res)});
 /*async function arrpush(ar1,ar2){
 var result=await arrpush(ar1,ar2);
 return result;
